e53417a07caa58b992e2d2756a7d45df822a79f5
skye7012 <skye7012@gmail.com>
2022-06-18 17:02:51 +0300

    I DID IT SHOES COUNT!

--- shoesDjangoApi/shoes/Serializers/ShoeSerializer.py
+++ shoesDjangoApi/shoes/Serializers/ShoeSerializer.py
@@ -8,9 +8,21 @@ from shoes.Serializers.SeasonSerializer import SeasonSerializer
 
 class ShoeSerializer(serializers.ModelSerializer):
 	brand = BrandSerializer()
-	season = SeasonSerializer()
 	destination = DestinationSerializer()
+	season = SeasonSerializer()
 
 	class Meta:
 		model = Shoe
-		fields = '__all__'
+		fields = ('id', 'name', 'image', 'price', 'brand', 'destination', 'season')
+
+
+# class GetShoeSerializer(serializers.Serializer):
+# 	count = serializers.IntegerField()
+# 	items = serializers.ListField()
+
+# class ShoeSerializer(serializers.Serializer):
+# 	# count = ShoeItemSerializer().data.count
+# 	items = ShoeItemSerializer(data=Shoe.objects.all()).validated_data
+#
+# 	# class Meta:
+# 	# 	fields = ('items',)
--- shoesDjangoApi/shoes/ViewSets/ShoeViewSet.py
+++ shoesDjangoApi/shoes/ViewSets/ShoeViewSet.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from shoes.Models.Shoe import Shoe
 from shoes.Serializers.ShoeSerializer import ShoeSerializer
@@ -7,4 +8,9 @@ from shoes.Serializers.ShoeSerializer import ShoeSerializer
 class ShoeViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = Shoe.objects.all()
 	serializer_class = ShoeSerializer
+
+	def list(self, request, *args, **kwargs):
+		items = self.serializer_class(self.queryset, many=True).data
+		response = {'count': len(items), 'items': items}
+		return Response(response)
 
