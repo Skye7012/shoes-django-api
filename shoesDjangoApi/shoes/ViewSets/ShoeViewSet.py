353d2ec46651761fceb37c9b424cec2802b8478a
skye7012 <skye7012@gmail.com>
2022-06-18 21:34:50 +0300

    made aside filtets almost

--- shoesDjangoApi/shoes/ViewSets/ShoeViewSet.py
+++ shoesDjangoApi/shoes/ViewSets/ShoeViewSet.py
@@ -26,10 +26,29 @@ class ShoeViewSet(viewsets.ReadOnlyModelViewSet):
 		limit = params.get('Limit')
 		order_by = params.get('OrderBy')
 		is_ascending = params.get('IsAscending', default="true")
+		brand_filters = params.getlist('BrandFilters')
+		destination_filters = params.getlist('DestinationFilters')
+		season_filters = params.getlist('SeasonFilters')
+
 
 		# Поиск по наименованию
 		if search_query:
 			self.queryset = self.queryset.filter(name__contains=search_query)
+
+		# Фильтрация по выбранным брэндам
+		if brand_filters:
+			brand_filters = [int(b) for b in brand_filters]
+			self.queryset = self.queryset.filter(brand_id__in=brand_filters)
+
+		# Фильтрация по выбранным назначениям
+		if destination_filters:
+			destination_filters = [int(d) for d in destination_filters]
+			self.queryset = self.queryset.filter(destination_id__in=destination_filters)
+
+		# Фильтрация по выбранным сезонам
+		if season_filters:
+			season_filters = [int(s) for s in season_filters]
+			self.queryset = self.queryset.filter(season_id__in=season_filters)
 
 		# Сортировка
 		if order_by and is_ascending:
