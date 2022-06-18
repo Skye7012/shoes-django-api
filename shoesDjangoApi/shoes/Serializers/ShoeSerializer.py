144f199e99d424629e609b530332d13037dc3ecd
skye7012 <skye7012@gmail.com>
2022-06-18 16:00:57 +0300

    shoe nested relations

--- shoesDjangoApi/shoes/Serializers/ShoeSerializer.py
+++ shoesDjangoApi/shoes/Serializers/ShoeSerializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
 from shoes.Models.Shoe import Shoe
+from shoes.Serializers.BrandSerializer import BrandSerializer
+from shoes.Serializers.DestinationSerializer import DestinationSerializer
+from shoes.Serializers.SeasonSerializer import SeasonSerializer
 
 
 class ShoeSerializer(serializers.ModelSerializer):
+	brand = BrandSerializer()
+	season = SeasonSerializer()
+	destination = DestinationSerializer()
+
 	class Meta:
 		model = Shoe
 		fields = '__all__'
