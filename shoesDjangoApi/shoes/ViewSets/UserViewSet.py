bd5ef0b8d7290cf294007b9e2d4b38dc3b37f4f6
skye7012 <skye7012@gmail.com>
2022-06-20 18:55:30 +0300

    create user

--- shoesDjangoApi/shoes/ViewSets/UserViewSet.py
+++ shoesDjangoApi/shoes/ViewSets/UserViewSet.py
@@ -1,3 +1,4 @@
+from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -9,36 +10,44 @@ from shoes.Serializers.UserSerializer import UserSerializer
 
 
 class UserViewSet(mixins.RetrieveModelMixin,
-					mixins.UpdateModelMixin,
-					mixins.DestroyModelMixin,
-					GenericViewSet):
+				  mixins.UpdateModelMixin,
+				  mixins.DestroyModelMixin,
+				  GenericViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
 	# permission_classes = [IsAuthenticated]
 
 	@action(detail=False, methods=['post'])
-	def sign_up(self, request):
-		data = request.data
-		user = User(
-			email=data['email'],
-			password=data['password'],
-			first_name=data['first_name'],
-			last_name=data['last_name'],
-			phone=data['phone'],
-		)
-		user.save()
-		return Response()
-		# serializer = PasswordSerializer(data=request.data)
-		# if serializer.is_valid():
-		# 	user.set_password(serializer.validated_data['password'])
-		# 	user.save()
-		# 	return Response({'status': 'password set'})
-		# else:
-		# 	return Response(serializer.errors,
-		# 					status=status.HTTP_400_BAD_REQUEST)
+	def signup(self, request):
+		data = request.POST
 
-	# def retrieve(self, request, *args, **kwargs):
-	# 	pk = kwargs['pk']
-	# 	user = User.objects.get(pk=pk)
-	# 	user = UserSerializer(user).data
-	# 	return Response(user)
+		try:
+			user = User(
+				email=data['email'],
+				password=data['password'],
+				first_name=data['first_name'],
+			)
+		except MultiValueDictKeyError as e:
+			return Response(f"Required field {str(e)} not specified")
+
+		user.last_name = data.get('last_name', '')
+		user.phone = data.get('phone', '')
+
+		user.save()
+		user_data = self.serializer_class(user).data
+		return Response(user_data)
+	# serializer = PasswordSerializer(data=request.data)
+	# if serializer.is_valid():
+	# 	user.set_password(serializer.validated_data['password'])
+	# 	user.save()
+	# 	return Response({'status': 'password set'})
+	# else:
+	# 	return Response(serializer.errors,
+	# 					status=status.HTTP_400_BAD_REQUEST)
+
+# def retrieve(self, request, *args, **kwargs):
+# 	pk = kwargs['pk']
+# 	user = User.objects.get(pk=pk)
+# 	user = UserSerializer(user).data
+# 	return Response(user)
