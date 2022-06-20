16a58ad6be21ff2d20a9ab58a65c6a9d0e22cc85
skye7012 <skye7012@gmail.com>
2022-06-20 19:59:05 +0300

    send pretty email with html template

--- shoesDjangoApi/shoes/ViewSets/UserViewSet.py
+++ shoesDjangoApi/shoes/ViewSets/UserViewSet.py
@@ -1,6 +1,9 @@
 import uuid
 
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import send_mail, EmailMessage, EmailMultiAlternatives
+from django.db import transaction
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import mixins
 from rest_framework.decorators import action
@@ -22,6 +25,7 @@ class UserViewSet(mixins.RetrieveModelMixin,
 
 	# permission_classes = [IsAuthenticated]
 
+	@transaction.atomic
 	@action(detail=False, methods=['post'])
 	def signup(self, request):
 		data = request.POST
@@ -40,31 +44,32 @@ class UserViewSet(mixins.RetrieveModelMixin,
 
 		user.save()
 
-		self.send_signup_email(user)
+		send_signup_email(user)
 
 		user_data = self.serializer_class(user).data
 		return Response(user_data)
 
-	def send_signup_email(self, user):
 
-		code = SignupCode(
-			code=uuid.uuid4(),
-			user=user
-		)
-		code.save()
+def send_signup_email(user):
 
-		subject = 'Signup verification'
-		body = 'Verify your email address by clicking on this link: link'
-		to = [user.email]
+	code = SignupCode(
+		code=uuid.uuid4(),
+		user=user
+	)
+	code.save()
 
-		email = EmailMessage(subject, body, to=to)
-		email.send()
-		# send_mail(
-		# 	subject='Signup verification',
-		# 	message='Verify your email address by clicking on this link: link',
-		# 	recipient_list=[user.email]
-		# )
+	subject = 'Signup verification'
+	body = f'Verify your email address by clicking on this link:\n{get_verify_link(code.code)}'
+	html_content = render_to_string("signup_email_template.html", {'code': code.code})
+	to = [user.email]
 
+	email = EmailMultiAlternatives(subject, body, to=to)
+	email.attach_alternative(html_content, "text/html")
+	email.send()
+
+
+def get_verify_link(code):
+	return f"http://127.0.0.1:8000/verify/?code={code}"
 
 	# serializer = PasswordSerializer(data=request.data)
 	# if serializer.is_valid():
